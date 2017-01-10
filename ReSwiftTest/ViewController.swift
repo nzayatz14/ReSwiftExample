7836c7c4ba6e110b69aa3231e70618e66b3fb5f0
Nick Zayatz <nzayatz@cirtual.com>
2017-01-10 14:48:43 -0600

    Initial Commit

---
+++ ReSwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReSwiftTest
+//
+//  Created by Nick Zayatz on 1/10/17.
+//  Copyright © 2017 Cirtual LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
